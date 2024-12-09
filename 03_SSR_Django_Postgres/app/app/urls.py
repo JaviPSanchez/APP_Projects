82435ed99e17789b9b4b4a6e480c2351f9b09001
JaviPSanchez <javier.palomino-sanchez@outlook.com>
2024-12-09 06:34:15 +0100

    feat: finish ingredients api

--- 03_SSR_Django_Postgres/app/app/urls.py
+++ 03_SSR_Django_Postgres/app/app/urls.py
@@ -1,23 +1,25 @@
 """
 URL mappings for the user app.
 """
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
 
 from django.contrib import admin
+
 # include (from different app)
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path(
-        'api/docs/',
-        SpectacularSwaggerView.as_view(url_name='api-schema'),
-        name='api-docs',
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
     ),
-    path('api/user/', include('user.urls')),
-    path('api/recipe/', include('recipe.urls')),
+    path("api/user/", include("user.urls")),
+    path("api/recipe/", include("recipe.urls")),
 ]
